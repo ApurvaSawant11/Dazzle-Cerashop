7ee20533622759821ecdf89ae178ffab9d296e79
Apurva Sawant <apurvasawant45@gmail.com>
2022-03-18 10:55:52 +0530

    remove : unnecessary fragments

--- src/components/card/categoryCard/CategoryCard.jsx
+++ src/components/card/categoryCard/CategoryCard.jsx
@@ -5,17 +5,15 @@ import { Link } from "react-router-dom";
 
 const CategoryCard = ({ img: { alt, url }, categoryName }) => {
   return (
-    <>
-      <div className="card-vertical category-item">
-        <span className="category-header mt-1p5">{categoryName}</span>
-        <img className="card-img-vertical" src={url} alt={alt} loading="lazy" />
-        <span className="overlay">
-          <Link to="/" className="button button-anchor">
-            View
-          </Link>
-        </span>
-      </div>
-    </>
+    <div className="card-vertical category-item">
+      <span className="category-header mt-1p5">{categoryName}</span>
+      <img className="card-img-vertical" src={url} alt={alt} loading="lazy" />
+      <span className="overlay">
+        <Link to="/" className="button button-anchor">
+          View
+        </Link>
+      </span>
+    </div>
   );
 };
 
--- src/components/card/productCard/ProductCard.jsx
+++ src/components/card/productCard/ProductCard.jsx
@@ -11,42 +11,40 @@ const ProductCard = ({ productDetails }) => {
     isBestSeller,
   } = productDetails;
   return (
-    <>
-      <div className="card-vertical m-1">
-        <Link to="/singleproduct" className="no-underline primary-text">
-          <div className="badge">
-            <img
-              loading="lazy"
-              className="card-img-vertical img-responsive"
-              src={url}
-              alt={alt}
-            />
+    <div className="card-vertical m-1">
+      <Link to="/singleproduct" className="no-underline primary-text">
+        <div className="badge">
+          <img
+            loading="lazy"
+            className="card-img-vertical img-responsive"
+            src={url}
+            alt={alt}
+          />
 
-            <span className="badge-text badge-on-left success px-0p5">
-              Bestseller
-            </span>
+          <span className="badge-text badge-on-left success px-0p5">
+            Bestseller
+          </span>
 
-            <span>
-              <i
-                className={
-                  isWishlisted
-                    ? "far fa-heart wishlist-btn wishlisted"
-                    : "far fa-heart wishlist-btn"
-                }
-              ></i>
-            </span>
+          <span>
+            <i
+              className={
+                isWishlisted
+                  ? "far fa-heart wishlist-btn wishlisted"
+                  : "far fa-heart wishlist-btn"
+              }
+            ></i>
+          </span>
+        </div>
+        <div className="card-details text-center">
+          <div className="title">{title}</div>
+          <div className="card-footer-vertical">
+            <span className="discountedPrice">Rs. {discounted}</span>
+            <span className="originalPrice pl-0p5">Rs. {original}</span>
           </div>
-          <div className="card-details text-center">
-            <div className="title">{title}</div>
-            <div className="card-footer-vertical">
-              <span className="discountedPrice">Rs. {discounted}</span>
-              <span className="originalPrice pl-0p5">Rs. {original}</span>
-            </div>
-          </div>
-        </Link>
-        <button className="plain-button card-button mt-0p5">Add to Cart</button>
-      </div>
-    </>
+        </div>
+      </Link>
+      <button className="plain-button card-button mt-0p5">Add to Cart</button>
+    </div>
   );
 };
 
